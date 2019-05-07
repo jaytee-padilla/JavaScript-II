cb5b7cce39b4e2472a036917be951690e66f0aec
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-05-07 15:47:02 -0700

    Project MVP complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -21,12 +21,11 @@ outer();
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+// Return a function that when invoked increments and returns a counter variable.
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
